b0d0b4b3630a0e117f263dc89c68d34129090835
kushal y kale <kushalykale7@gmail.com>
2025-04-01 18:51:01 +0530

    new file

---
+++ src/main/java/com/DevScribe/ui/components/EditorHandler.java
@@ -0,0 +1,56 @@
+package com.DevScribe.ui.components;
+
+import javafx.stage.Stage;
+
+
+public class EditorHandler {
+
+
+    public void handleNewFile(Stage primaryStage) {
+        // Implementation for new file
+    }
+
+    public void handleOpenFile() {
+        // Implementation for open file
+    }
+
+    public void handleSaveFile() {
+        // Implementation for save file
+    }
+
+    public void handleSaveAsFile() {
+        // Implementation for save as file
+    }
+
+    public void handleUndo() {
+        // Implementation for undo
+    }
+
+    public void handleRedo() {
+        // Implementation for redo
+    }
+
+    public void handleCut() {
+        // Implementation for cut
+    }
+
+    public void handleCopy() {
+        // Implementation for copy
+    }
+
+    public void handlePaste() {
+        // Implementation for paste
+    }
+
+    public void handleZoomIn() {
+        // Implementation for zoom in
+    }
+
+    public void handleZoomOut() {
+        // Implementation for zoom out
+    }
+
+    public void handleWordWrap(boolean selected) {
+        // Implementation for word wrap toggle
+    }
+}
